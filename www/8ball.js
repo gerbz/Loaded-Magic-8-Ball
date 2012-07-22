05fe7f2d01ca31e38a8a25698d0e5b748731b97e
GG <gerber.greg@gmail.com>
2012-07-22 13:01:33 -0700

    iOSDevCamp Demo Release Candidate 1
    - iPhone & iPad springboard and splash images
    - retina/non-retina messages added
    -- 3 yes
    -- 3 no
    -- 2 maybes
    - Fixed bug where maybe got stuck

--- www/8ball.js
+++ www/8ball.js
@@ -9,7 +9,8 @@ TO-DO
 -More yes and no options based on number of spins
 -Some maybe answers randomly
 
-
+BUGS
+-Holding in landscape acts nuts
 
 */
 
@@ -18,6 +19,12 @@ TO-DO
 	var sameShakeCount = 0;
 	var gameOn;
 	var lastShake;
+	var maybe = false;
+
+// Random number generator
+function getRandomInt (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 	
     // Start watching the acceleration
     //
@@ -98,24 +105,89 @@ TO-DO
         	sameShakeCount++;
         	
         	// If they do not shake for 1 second, they are done shaking
-        	if(sameShakeCount > 10){
+        	if(sameShakeCount > 10 && gameOn == true){
 
+        		// Get the image element and recenter it
+        		var theMsgY = document.getElementById("message");
+        		theMsgY.setAttribute("style", "margin-top:-90px;");
+
+				//Get a random number to determine which message to display
+				var random = getRandomInt(0, 4);
+				console.log(random);
+				
+				// Display the maybe message if # is 0 or 4
+				var maybe = false;
+				if(random == 4 || random == 0){
+				
+					var maybe = true;
+					
+        			// Pick the message based on the random number
+        			switch(random){
+						case 0:
+						  console.log('maybe');
+						  theMsgY.setAttribute("class", "msg1m");
+						  break;
+						case 4:
+						  console.log('maybe');
+						  theMsgY.setAttribute("class", "msg2m");
+						  break;
+						default:
+						  console.log('maybe');
+						  theMsgY.setAttribute("class", "msg2m");
+						  break;
+					}
+				}
+
+				
         		// If they shook more than the default, the answer is yes
-        		if(numberOfShakes > loadedDefault){
+        		if(numberOfShakes > loadedDefault && maybe != true){
         			console.log('yes!');
-        			        			
-        			var theMsgY = document.getElementById("message");
-					theMsgY.setAttribute("class", "msg1y");
-					theMsgY.setAttribute("style", "margin-top:-90px;");
+			
+        			// Pick the message based on the random number
+        			switch(random){
+						case 1:
+						  console.log('yeah totally');
+						  theMsgY.setAttribute("class", "msg1y");
+						  break;
+						case 2:
+						  console.log('thats correct');
+						  theMsgY.setAttribute("class", "msg2y");
+						  break;
+						case 3:
+						  console.log('fosho!');
+						  theMsgY.setAttribute("class", "msg3y");
+						  break;
+						default:
+						  console.log('fosho!');
+						  theMsgY.setAttribute("class", "msg3y");
+						  break;
+					}				
         			 
         		// If they shook less than the default, the answer is no
         		// Dont set it to anything when the app first loads (>3)        		
-        		}else if(numberOfShakes > 3 && numberOfShakes < loadedDefault){
+        		}else if(numberOfShakes > 3 && numberOfShakes < loadedDefault && maybe != true){
         			console.log('no...');
-        			
-        			var theMsgN = document.getElementById("message");
-					theMsgN.setAttribute("class", "msg1n");
-					theMsgN.setAttribute("style", "margin-top:-90px;");
+        			var maybe = false;
+        		
+        			// Pick the message based on the random number
+        			switch(random){
+						case 1:
+						  console.log('no way');
+						  theMsgY.setAttribute("class", "msg1n");
+						  break;
+						case 2:
+						  console.log('I think not');
+						  theMsgY.setAttribute("class", "msg2n");
+						  break;
+						case 3:
+						  console.log('not a chance');
+						  theMsgY.setAttribute("class", "msg3n");
+						  break;
+						default:
+						  console.log('not a chance');
+						  theMsgY.setAttribute("class", "msg3n");
+						  break;
+					}
         		}
         		
         		// Reset for a fresh shake
