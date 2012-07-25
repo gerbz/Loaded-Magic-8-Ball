67469f1b5f98138ed647244c8ee82048943dd6f0
GG <gerber.greg@gmail.com>
2012-07-25 13:50:28 -0700

    1.0.0
    - added a readme file
    - added an #iOSDevCamp easter egg for presentation =)
    - renamed and relocated homescreen & splash screen images to the root
    directory
    - modify image source instead of swapping classes when message changes
    - added global debug option

---
+++ README.md
@@ -0,0 +1,30 @@
+# Loaded Magic Eightball
+
+	Needed!
+	Some awesome new design
+
+#### How it works
+Having a tough time convincing someone to go with your recommendation?  Rock, paper, scissors and coin flips are for n00bs.  Everyone trusts the Magic 8 Ball, but should they...
+
+Shake the Loaded Magic 8 Ball for more than 3 seconds to get a yes, less than 3 seconds for a no.  Maybe's will happen... maybe.
+
+####Requirements
+Don't forget to swap out the entitlements if you want to make a build for your device
+
+[PhoneGap 1.9.0](http://phonegap.com/download)
+
+[Xcode](https://developer.apple.com/xcode/)
+
+#### Issues
+Report problems and submit feature requests on our [GitHub issue tracker](https://github.com/ggwarpig/Loaded-Magic-8-Ball/issues)
+
+#### ToDo
+-iPhone support.  Images and css (currently only iPad)
+
+-Replace image messages with text messages 
+#### Known Bugs
+-Holding in landscape spins uncontrolably
+
+---
+
+Ideaized by [@nicklum](http://twitter.com/nicklum) developed by [@greg_gerber](http://twitter.com/greg_gerber) at [#iOSDevCamp2012](http://iosdevcamp.org)
--- www/8ball.js
+++ www/8ball.js
@@ -1,207 +1,206 @@
-/*
-TO-DO
--iPad App icpm
--iPad splash screen
-
--iPhone App icon
--iPhone splash screen
-
--More yes and no options based on number of spins
--Some maybe answers randomly
-
-BUGS
--Holding in landscape acts nuts
-
-*/
-
-	var loadedDefault = 15;
-	var numberOfShakes = 0;
-	var sameShakeCount = 0;
-	var gameOn;
-	var lastShake;
-	var maybe = false;
+var debug = false;
+var loadedDefault = 15;
+var numberOfShakes = 0;
+var sameShakeCount = 0;
+var gameOn;
+var lastShake;
+var easter;
 
 // Random number generator
+//http://stackoverflow.com/a/1527820/345599
 function getRandomInt (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-	
-    // Start watching the acceleration
-    //
-    function startWatch() {
 
-        // Update acceleration every 3 seconds
-        var options = { frequency: 100 };
+// Start watching the acceleration
+//
+function startWatch() {
 
-        watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+    // Update acceleration every 3 seconds
+    var options = { frequency: 100 };
+
+    watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+}
+
+// Stop watching the acceleration
+//
+function stopWatch() {
+    if (watchID) {
+        navigator.accelerometer.clearWatch(watchID);
+        watchID = null;	
+    }
+}
+
+// onSuccess: Get a snapshot of the current acceleration
+//
+function onSuccess(acceleration) {
+    
+    //DEBUG display the accelerometer info
+    if(debug){
+	    var element = document.getElementById('accelerometer');
+	    element.innerHTML = 'Acceleration X: ' + acceleration.x + '<br />' +
+	                        'Acceleration Y: ' + acceleration.y + '<br />' +
+	                        'Acceleration Z: ' + acceleration.z + '<br />' +
+	                        'Timestamp: '      + acceleration.timestamp + '<br />';
+    
+    	// show the # of shakes
+    	var shakes = document.getElementById('shakes');
+    	shakes.innerHTML = numberOfShakes;
+    
     }
 
-    // Stop watching the acceleration
-    //
-    function stopWatch() {
-        if (watchID) {
-            navigator.accelerometer.clearWatch(watchID);
-            watchID = null;	
-        }
-    }
+    // Start counting shakes on the first good "hard" shake
+    if(gameOn == true || acceleration.x > 4){
 
-    // onSuccess: Get a snapshot of the current acceleration
-    //
-    function onSuccess(acceleration) {
-        
-        // DEBUG display the accelerometer info
-        //var element = document.getElementById('accelerometer');
-        //element.innerHTML = 'Acceleration X: ' + acceleration.x + '<br />';
-                            //'Acceleration Y: ' + acceleration.y + '<br />' +
-                            //'Acceleration Z: ' + acceleration.z + '<br />' +
-                            //'Timestamp: '      + acceleration.timestamp + '<br />';
-
-		// Start counting shakes on the first good "hard" shake
-		if(gameOn == true || acceleration.x > 4){
-			
-			// Spin the image if its not already spinning
-			var msgClass = document.getElementById("message").getAttribute("class");
-			if(msgClass != 'msgBlank'){
-				var theMsg = document.getElementById("message");
-				theMsg.setAttribute("class", "msgBlank");
-				theMsg.setAttribute("style", "margin-top:-120px;");
-				
-			}
-			
-			// Once we start counting, the shakes dont need to be so hard to count
-			gameOn = true;
-			
-			// A "shake" means it crossed the x2 threshold
-			var shake;
-	        if(acceleration.x > 2){
-	        	shake = true;
-	        }else if(acceleration.x < 2){
-	        	shake = false;
-	        }
-        }
+    	// Once we start counting, the shakes dont need to be so hard to count
+    	gameOn = true;
 		
-		// Pass the shake to the counter
-        countShakes(shake);
-
-    }
-
-    // onError: Failed to get the acceleration
-    //
-    function onError() {
-        alert('onError!');
-    }
-
-	
-	function countShakes(shake){
-		
-		// Count up the shakes if its different from the last shake
-		// If the shake changed, reset the same shake counter
-        if(shake != lastShake){
-        	numberOfShakes++;
-        	sameShakeCount = 0;
-        }else{
-        	
-        	// Count how many times a shake did _not_ happen
-        	sameShakeCount++;
-        	
-        	// If they do not shake for 1 second, they are done shaking
-        	if(sameShakeCount > 10 && gameOn == true){
-
-        		// Get the image element and recenter it
-        		var theMsgY = document.getElementById("message");
-        		theMsgY.setAttribute("style", "margin-top:-90px;");
-
-				//Get a random number to determine which message to display
-				var random = getRandomInt(0, 4);
-				console.log(random);
-				
-				// Display the maybe message if # is 0 or 4
-				var maybe = false;
-				if(random == 4 || random == 0){
-				
-					var maybe = true;
-					
-        			// Pick the message based on the random number
-        			switch(random){
-						case 0:
-						  console.log('maybe');
-						  theMsgY.setAttribute("class", "msg1m");
-						  break;
-						case 4:
-						  console.log('maybe');
-						  theMsgY.setAttribute("class", "msg2m");
-						  break;
-						default:
-						  console.log('maybe');
-						  theMsgY.setAttribute("class", "msg2m");
-						  break;
-					}
-				}
-
-				
-        		// If they shook more than the default, the answer is yes
-        		if(numberOfShakes > loadedDefault && maybe != true){
-        			console.log('yes!');
-			
-        			// Pick the message based on the random number
-        			switch(random){
-						case 1:
-						  console.log('yeah totally');
-						  theMsgY.setAttribute("class", "msg1y");
-						  break;
-						case 2:
-						  console.log('thats correct');
-						  theMsgY.setAttribute("class", "msg2y");
-						  break;
-						case 3:
-						  console.log('fosho!');
-						  theMsgY.setAttribute("class", "msg3y");
-						  break;
-						default:
-						  console.log('fosho!');
-						  theMsgY.setAttribute("class", "msg3y");
-						  break;
-					}				
-        			 
-        		// If they shook less than the default, the answer is no
-        		// Dont set it to anything when the app first loads (>3)        		
-        		}else if(numberOfShakes > 3 && numberOfShakes < loadedDefault && maybe != true){
-        			console.log('no...');
-        			var maybe = false;
-        		
-        			// Pick the message based on the random number
-        			switch(random){
-						case 1:
-						  console.log('no way');
-						  theMsgY.setAttribute("class", "msg1n");
-						  break;
-						case 2:
-						  console.log('I think not');
-						  theMsgY.setAttribute("class", "msg2n");
-						  break;
-						case 3:
-						  console.log('not a chance');
-						  theMsgY.setAttribute("class", "msg3n");
-						  break;
-						default:
-						  console.log('not a chance');
-						  theMsgY.setAttribute("class", "msg3n");
-						  break;
-					}
-        		}
-        		
-        		// Reset for a fresh shake
-        		gameOn = false;
-        		numberOfShakes = 0;
-        		
-        	}
-        	//console.log(sameShakeCount);
+    	// Spin the blank image
+    	var msgDiv = document.getElementById("message");
+    	msgDiv.setAttribute("class", "msgSpin");
+    	msgDiv.setAttribute("style", "margin-top:-120px;");
+    	document.getElementById("msgimg").setAttribute("src", "img/iPadMessageBlank.png");
+    	
+    	// A "shake" means it crossed the x2 threshold
+    	var shake;
+        if(acceleration.x > 2){
+        	shake = true;
+        }else if(acceleration.x < 2){
+        	shake = false;
         }
-        
-        // Remember the last shake for next time
-        lastShake = shake;
-        
-        // Debug code to show the # of shakes
-        //var shakes = document.getElementById('shakes');
-        //shakes.innerHTML = numberOfShakes;
-	}
+    }
+    
+    // Pass the shake to the counter
+    countShakes(shake);
+
+}
+
+// onError: Failed to get the acceleration
+//
+function onError() {
+    alert('onError!');
+}
+
+
+// Easter egg!
+function egg(){
+	easter = true;
+	console.log('egg hit!');
+}
+
+function countShakes(shake){
+    
+    // Count up the shakes if its different from the last shake
+    // If the shake changed, reset the same shake counter
+    if(shake != lastShake){
+    	numberOfShakes++;
+    	sameShakeCount = 0;
+    }else{
+    	
+    	// Count how many times a shake did _not_ happen
+    	sameShakeCount++;
+    	
+    	// If they do not shake for 1 second, they are done shaking
+    	if(sameShakeCount > 10 && gameOn == true){
+			
+			// Stop spinning and recenter
+			var msgDiv = document.getElementById("message");
+			msgDiv.setAttribute("style", "");
+			msgDiv.setAttribute("class", "");
+			
+			// Get the image element to swap it out later
+			var msgImg = document.getElementById("msgimg");
+
+    		//Get a random number to determine which message to display
+    		var random = getRandomInt(0, 4);
+    		console.log(random);
+    		
+    		// Begin deciding which message to display
+    		// Easter egg trumps all options
+    		if(easter == true){
+    		    console.log('egg');
+    			msgImg.setAttribute("src", "img/iPadMessage1m.png");
+    			easter = false;
+    		
+    		}
+    		// Display the maybe message if # is 0 or 4
+    		else if(random == 4 || random == 0){
+    			
+    			// Pick the message based on the random number
+    			switch(random){
+    				case 0:
+    				  console.log('maybe');
+    				  msgImg.setAttribute("src", "img/iPadMessage1m.png");
+    				  break;
+    				case 4:
+    				  console.log('maybe');
+    				  msgImg.setAttribute("src", "img/iPadMessage2m.png");
+    				  break;
+    				default:
+    				  console.log('maybe');
+    				  msgImg.setAttribute("src", "img/iPadMessage3m.png");
+    				  break;
+    			}
+    			
+    		// If they shook more than the default, the answer is yes
+    		}else if(numberOfShakes > loadedDefault){
+    			console.log('yes!');
+    	
+    			// Pick the message based on the random number
+    			switch(random){
+    				case 1:
+    				  console.log('yeah totally');
+    				  msgImg.setAttribute("src", "img/iPadMessage1y.png");
+    				  break;
+    				case 2:
+    				  console.log('thats correct');
+    				  msgImg.setAttribute("src", "img/iPadMessage2y.png");
+    				  break;
+    				case 3:
+    				  console.log('fosho!');
+    				  msgImg.setAttribute("src", "img/iPadMessage3y.png");
+    				  break;
+    				default:
+    				  console.log('fosho!');
+    				  msgImg.setAttribute("src", "img/iPadMessage3y.png");
+    				  break;
+    			}				
+    			 
+    		// If they shook less than the default, the answer is no
+    		// Dont set it to anything when the app first loads (>3)        		
+    		}else if(numberOfShakes > 3 && numberOfShakes < loadedDefault){
+    			console.log('no...');
+    		
+    			// Pick the message based on the random number
+    			switch(random){
+    				case 1:
+    				  console.log('no way');
+    				  msgImg.setAttribute("src", "img/iPadMessage1n.png");
+    				  break;
+    				case 2:
+    				  console.log('I think not');
+    				  msgImg.setAttribute("src", "img/iPadMessage2n.png");
+    				  break;
+    				case 3:
+    				  console.log('not a chance');
+    				  msgImg.setAttribute("src", "img/iPadMessage3n.png");
+    				  break;
+    				default:
+    				  console.log('not a chance');
+    				  msgImg.setAttribute("src", "img/iPadMessage3n.png");
+    				  break;
+    			}
+    		}
+    		
+    		// Reset for a fresh shake
+    		gameOn = false;
+    		numberOfShakes = 0;
+    		
+    	}
+    	//console.log(sameShakeCount);
+    }
+    
+    // Remember the last shake for next time
+    lastShake = shake;
+
+}
